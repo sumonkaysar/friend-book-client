9e4f142865e192ca46a292f6e45da173aa0b9392
Sumon Kaysar <msk.sumon.kaysar@gmail.com>
2022-12-28 00:51:10 +0600

    changes made

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,23 +3,44 @@ import { FaTimes } from "react-icons/fa";
 
 const Home = () => {
   const [images, setImages] = useState([]);
-  let uploaded_images = [];
 
   const handlePost = e => {
     e.preventDefault();
 
-    const form = e.target;
+    const text = e.target.textPost;
+    const uploaded_images = [];
+    images.forEach((image, i) => {
+      const formData = new FormData();
+      formData.append(`image`, image);
+        fetch(`https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_imgbb_key}`, {
+        method: 'POST',
+        body: formData
+      })
+      .then(res => res.json())
+      .then(imgData => {
+        console.log(imgData);
+        if (imgData?.status === 200) {
+          uploaded_images.push(imgData?.data?.display_url);
+        }
+      }).catch(err => console.error(err))
+    });
+    console.log(uploaded_images);
+    // fetch()
   }
   const handleImageUpload = e => {
+    const uploaded_images = [];
     for (const image of e.target.files) {
-      uploaded_images.push(URL.createObjectURL(image));
+      uploaded_images.push(image);
     }
     setImages(uploaded_images);
   }
   const handleRemoveImage = index => {
-    uploaded_images = images.filter((_, i) => index !== i);
-    console.log(index, uploaded_images);
-    setImages(uploaded_images);
+    const remained_images = images.filter((_, i) => index !== i);
+    setImages(remained_images);
+  }
+
+  const handleClick = e => {
+    e.target.value = null;
   }
 
   return (
@@ -32,25 +53,27 @@ const Home = () => {
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                {/* <textarea className="input input-bordered" name="" id="" cols="30" rows="10"></textarea> */}
-                <textarea className="textarea textarea-bordered" placeholder="Bio"></textarea>
+                <textarea name="textPost" className="textarea textarea-bordered" placeholder="Bio"></textarea>
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input onChange={handleImageUpload} name="image" type="file" className="file-input file-input-bordered w-full" accept="image/*" multiple />
+                <input onClick={handleClick} onChange={handleImageUpload} name="image" type="file" className="file-input file-input-bordered w-full" accept="image/*" multiple />
               </div>
-              <div className="grid grid-cols-5">
+              <div className="flex flex-wrap gap-3">
                 {
-                  images && images.map((img, i) => <div className="relative" key={i}>
+                  images && images.map((img, i) => <div
+                    className="relative w-24 h-24 overflow-hidden"
+                    key={i}
+                  >
                     <img
-                      className="h-24 w-24 object-cover"
-                      src={img}
+                      className="w-full h-full object-cover"
+                      src={URL.createObjectURL(img)}
                     />
                     <span
                       onClick={() => { handleRemoveImage(i) }}
-                      className="absolute top-1 right-6 cursor-pointer text-sm text-white"
+                      className="absolute top-1 right-1 cursor-pointer text-sm text-white"
                     ><FaTimes /></span>
                   </div>)
                 }
